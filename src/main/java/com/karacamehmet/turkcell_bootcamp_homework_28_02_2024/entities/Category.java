18ccd7d58acf4c1ab07ea5a1e9996e26c9300c48
karaca <mekaraca18@gmail.com>
2024-02-28 21:24:39 +0300

    Category

--- src/main/java/com/karacamehmet/turkcell_bootcamp_homework_28_02_2024/entities/Category.java
+++ src/main/java/com/karacamehmet/turkcell_bootcamp_homework_28_02_2024/entities/Category.java
@@ -2,8 +2,11 @@ package com.karacamehmet.turkcell_bootcamp_homework_28_02_2024.entities;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Category {
     private int id;
     private String name;
+    private List<Integer> productIds;
 }
